1f4ae5cda9c82f27d9005e66d71d63148a4d2cb0
jensyfranco <jenmilafran@gmail.com>
2023-03-29 14:23:50 +0200

    feature huiswerkopdracht les 7

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -14,8 +14,20 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan bijhouden?
 // Log het antwoord in de terminal.
 
+//Stappen en aandachtspunten:
+//1: Input is cijfers van studenten. De array is al aangemaakt, array is grades.
+//2: De cijfers van de studenten moeten gecontroleerd worden door te itereren. Output moet het aantal studenten met een cijfer 8 of hoger zijn. Wanneer er een 8 of hoger vinden (if statement gebruiken) dan geldt dit als een cum laude en horen we dit bij te houden (we houden alle cum laude bij).
+//3: De uitslag moet bewaard worden in een nieuwe variable result of highGrades.
+//4: Aantal studenten met een cijfer 8 of hoger moet gelogged worden in de terminal via console.log.
+
 // ---- Verwachte uitkomst: 6
 
+//In de opdrachten gebruik ik arr (als afkorting voor array) als parameter
+//Arrow funtie en filter gebruikt om de code kort te houden
+const highScore = grades.filter(grades => grades >= 8);
+//Variable highScore resulteert in array [9, 8, 9, 8, 8, 8]. We willen weten wat de lengte is van deze array dus moeten we highScore.length loggen.
+console.log(highScore); //De nieuwe array na het toepassen van een filter
+console.log(highScore.length);
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
@@ -27,8 +39,17 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
-
-
+//filter() functie gebruikt om door de array te filteren en te zoeken naar cijfers van 8 en hoger.
+function cumLaude(arr) {
+    const highGrades = arr.filter(grades => grades >= 8);
+    console.log("Number of students who will receive a Cum Laude diploma: " + highGrades.length);
+    return highGrades;
+}
+//Resultaat als we de grades array gebruiken
+console.log(cumLaude(grades));
+//Resultaat als we een andere array gebruiken
+console.log(cumLaude([6, 4, 5]));
+console.log(cumLaude([8, 9, 4, 6, 10]));
 
 /* Opdracht  2: Gemiddeld cijfer */
 
@@ -39,6 +60,24 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // * Wat moet ik verzamelen uit de array van cijfers om uiteindelijk een gemiddelde te kunnen berekenen?
 // * Hoe zorgt ik ervoor dat ik alle waardes uit de array kan langslopen, ook als de array wel 100 entries zou bevatten?
 // Log het antwoord in de terminal.
+
+//Stappen en aandachtspunten:
+//1: Eerst moet de array gecontroleerd worden. Wat zijn de individuele waardes van de array.
+//2: Vervolgens moet de length van de array gecontroleerd worden.
+//3: Nu we zowel de lengte als de individuele waardes hebben kunnen we een berekening maken.
+//4: De individuele waardes moeten allemaal bij elkaar opgeteld worden.
+//5: Vervolgens moet de som van de individuele waardes gedeeld worden door het aantal waardes in de array, de length.
+//6: Het resultaat van som gedeeld door waarde moet worden gelogged in de terminal via console.log.
+
+//Aantal studenten in de array, geprint voor mijn eigen visualisatie
+const totalStudents = grades.length;
+console.log("Total students: " + totalStudents);
+let result = 0;
+
+for (let i = 0; i < grades.length; i++) {
+    result += grades[i] / 14;
+}
+console.log("The average grade this year is: " + result);
 
 // ---- Verwachte uitkomst: 6.642857142857143
 
@@ -53,12 +92,26 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // averageGrade([6, 4, 5]) geeft xxxx
 // averageGrade([8, 9, 4, 6, 10]) geeft xxxx
 
+function averageGrade(arr) {
+    const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    const resultAverage = sum / arr.length;
+    return resultAverage;
+}
+//In deze opdracht ervoor gekozen om averageGrade op te slaan in een variable en vervolgens te loggen.
+const averageGradeThisYear = averageGrade(grades);
+console.log(averageGradeThisYear);
+//Test met andere arrays
+console.log(averageGrade([6, 4, 5]));
+console.log(averageGrade([8, 9, 4, 6, 10]));
 
 /* 2c: Afronden op twee decimalen */
 // Zorg ervoor dat het gemiddelde cijfer dat wordt teruggegeven uit de functie netjes wordt afgerond op twee decimalen.
 // Tip: Google is your best friend!
 
-
+//Bij functie toFixed kun je een getal (fractionDigits) invoeren, is het aantal decimalen.
+//Dit is een korte versie, mag het ook zo geschreven worden of moet hier een functie(){} voor geschreven worden?
+const roundedAverage = (averageGrade(grades)).toFixed(2);
+console.log(roundedAverage);
 
 
 /* Bonusopdracht: hoogste cijfer */
@@ -70,7 +123,17 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan opslaan?
 // Log het antwoord in de terminal.
 
+//Stappen en aandachtspunten
+//Stap 1: We moeten alle cijfers in de array controleren op hun specifieke waarde (hoogte van de cijfers/hoogste getal).
+//Stap 2: Vervolgens moeten we controleren wat de hoogste cijfer is in de array door middel van een functie.
+//Stap 3: De uitkomst moet worden gelogged via een console.log
+
+
 // ---- Verwachte uitkomst: 9
+
+//Functie Math.max.apply gebruikt, mag dit?
+const highestStudentGrade = Math.max.apply(null, grades);
+console.log(highestStudentGrade);
 
 
 /* 3b: Omschrijven tot een herbruikbare functie */
@@ -82,3 +145,27 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // highestGrade(grades) geeft 9
 // highestGrade([6, 4, 5]) geeft 6
 // highestGrade([8, 9, 4, 6, 10]) geeft 10
+
+function highestGrade(arr) {
+    let highest = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > highest) {
+            highest = arr[i];
+        }
+    }
+    return highest;
+}
+const resultHighestGrade = highestGrade(grades);
+console.log(resultHighestGrade);
+//Test met andere arrays
+console.log(highestGrade([6, 4, 5]));
+console.log(highestGrade([8, 9, 4, 6, 10]));
+
+//Kan ook zo uitgevoerd worden
+/*function highestGrade(arr) {
+
+    const highestStudentGrade = Math.max.apply(null, arr);
+    return highestStudentGrade;
+}
+console.log(highestGrade([8, 9, 4, 6, 10]));*/
--- functions-methods.js
+++ functions-methods.js
@@ -9,8 +9,15 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
-
-
+function getEmailDomain(email) {
+    //Eerst moet de index van de @ teken gevonden worden, verschilt namelijk per emailadres.
+    const indexDomain = email.indexOf('@');
+    //Onderstaande print de string achter het @ symbool, dus de domeinnamen.
+    return email.slice(indexDomain + 1);
+}
+console.log(getEmailDomain("n.eeken@novi-education.nl"));
+console.log(getEmailDomain("t.mellink@novi.nl"));
+console.log(getEmailDomain("a.wiersma@outlook.com"));
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
@@ -20,7 +27,25 @@
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+function typeOfEmail(email) {
+    const emailType = email;
 
+    //Match functie gebruikt om de domeinnamen te controleren
+    //Gekozen om niet een else if voor de ..@outlook.com mailadressen te maken maar een else voor alles wat niet match met @novi-education en @novi.nl. Dit gaat waarschijnlijk problemen opleveren als een user invalide tekst invoert (bijv. geen email etc.)??
+    if (emailType.match("@novi-education.nl")) {
+        console.log("Student");
+    } else if (emailType.match("@novi.nl")) {
+        console.log("Medwerker")
+    } else {
+        console.log("Extern");
+    }
+    return emailType;
+}
+typeOfEmail("n.eeken@novi-education.nl");
+typeOfEmail("t.mellink@novi.nl");
+typeOfEmail("novi.nlaapjesk@outlook.com");
+typeOfEmail("a.wiersma@outlook.com");
+// Kan ook aan de hand van een console.log worden gelogged: console.log(typeOfEmail("a.wiersma@outlook.com"));
 
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
@@ -34,3 +59,19 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity(email) {
+    const validEmailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    //Controleert een email op juiste format via standaard richtlijnen en te controleren op specifieke tekens.
+    //Dit had waarschijnlijk ook met includes() opgelost kunnen worden, maar de code hierboven is korter.
+    if (email.match(validEmailFormat)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(checkEmailValidity("n.eeken@novi.nl"));
+console.log(checkEmailValidity("tessmellink@novi.nl"));
+console.log(checkEmailValidity("n.eekenanovi.nl"));
+console.log(checkEmailValidity("n.eeken@novinl."));
+console.log(checkEmailValidity("tessmellink@novi,nl"));
